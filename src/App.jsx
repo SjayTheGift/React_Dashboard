44efdd852bde786e0c232f000ad29aa28fa12904
Esethu <jonas.esethu24@gmail.com>
2023-07-17 04:01:48 +0200

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    <>
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SideNavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideNavBar = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+        Side Bar
+        </h1>
+    </div>
+  )
+}
+
+export default SideNavBar

114ece823ee54fa20e6941c4e84f1e1d7d5a851b
Esethu <jonas.esethu24@gmail.com>
2023-08-08 00:58:17 +0200

    back to original

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { decodeToken  } from "react-jwt";
-import { fetchHomeData} from '../features/home/homeActions'
-import LoadingSpinner  from '../components/LoadingSpinner'
+import axios from 'axios'
 
 const Home = () => {
 
@@ -10,30 +9,29 @@ const Home = () => {
     const { userToken }  = useSelector((state) => state.auth)
     const token = JSON.parse(userToken)
     const data = decodeToken(token.access)
+    const [userTotalData, setUserTotalData] = useState([])
+    const backendURL = import.meta.env.VITE_LOCAL_BACKEND_URL
 
-    const { homeData, isLoading, isSuccess} = useSelector((state) => state.home)
-    const [totalData, setTotalData] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
-    const dispatch = useDispatch()
 
-    
-
-    console.log(data)
-
-    
+    const fetchData = async () => {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      await axios.get(`${backendURL}/api/data/count/`, config)
+      .then(res => {
+            setUserTotalData(res.data)
+            setIsLoading(false)
+          return res.data
+      })
+    }
 
     useEffect(()=>{
-      dispatch(fetchHomeData())
-
-      if(isSuccess){
-        setTotalData(homeData)
-      }
-
-
-    },[homeData, isSuccess])
-
-    
-    let count_data = JSON.parse(totalData)
+        fetchData()
+    },[])
 
   return (
     <>
@@ -43,45 +41,32 @@ const Home = () => {
                 <h2 className="font-semibold text-slate-800 dark:text-slate-100">Start managing your employees from anywhere.</h2>
             </header>
         </div>
-        {isLoading
-        ?
-            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                <LoadingSpinner />
-            </div>
-        :
+
+        {!isLoading && 
         
-        <>
-        
-        {count_data.map((count) => 
             <>
                 <div className="col-span-full mr-8 md:mr-0 md:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
                     <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
                         <h2 className="font-semibold text-slate-800 dark:text-slate-100">Total Departments</h2>
-                        <h2 className="font-semibold text-slate-800 dark:text-slate-100">{count.total_department}</h2>
+                        <h2 className="font-semibold text-slate-800 dark:text-slate-100">{userTotalData[0].total_department}</h2>
                     </header>
                 </div>
 
                 <div className="col-span-full mr-8 md:mr-0 md:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
                     <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
                         <h2 className="font-semibold text-slate-800 dark:text-slate-100">Total Employees</h2>
-                        <h2 className="font-semibold text-slate-800 dark:text-slate-100">{count.total_users}</h2>
+                        <h2 className="font-semibold text-slate-800 dark:text-slate-100">{userTotalData[0].total_users}</h2>
                     </header>
                 </div>
 
                 <div className="col-span-full mr-8 md:mr-0 md:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
                     <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700">
                         <h2 className="font-semibold text-slate-800 dark:text-slate-100">Pending Leaves</h2>
-                        <h2 className="font-semibold text-slate-800 dark:text-slate-100">{count.total_pending_leave}</h2>
+                        <h2 className="font-semibold text-slate-800 dark:text-slate-100">{userTotalData[0].total_pending_leave}</h2>
                     </header>
                 </div>
             </>
-        
-        )}
-        
-
-        </>
         }
-                
     </>
   )
 }
